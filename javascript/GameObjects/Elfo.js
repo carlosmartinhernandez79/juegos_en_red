94432eefe1ccd674e14dfd5227c1950869298676
carlosmartinhernandez79 <147002686+carlosmartinhernandez79@users.noreply.github.com>
2023-12-10 04:28:20 +0100

    Add files via upload

--- javascript/GameObjects/Elfo.js
+++ javascript/GameObjects/Elfo.js
@@ -9,11 +9,13 @@ class Elfo extends Phaser.GameObjects.Sprite{
 
         this.elfo.scene.physics.world.enableBody(this);
 
-        this.elfo.setScale(0.7);
+        //this.elfo.setScale(0.7);
 
         this.flechas = scene.input.keyboard.createCursorKeys();
 
         this.canDoubleJump = 0;
+
+        this.lookLeft = true;
 
         this.metamorfosis = false;
 
@@ -23,25 +25,47 @@ class Elfo extends Phaser.GameObjects.Sprite{
         //const isJumpJustDown = Phaser.Input.Keyboard.JustDown(this.flechas.up)
 
         //ANIMATIONS DEL ELFO
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('elfa', { start: 27, end: 53 }),
-            frameRate: 24,
-            repeat: -1
-        });
         
         this.anims.create({
-            key: 'turn',
+            key: 'turnRight',
             frames: [ { key: 'elfa', frame: 0 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'turnLeft',
+            frames: [ { key: 'elfa', frame: 56 } ],
             frameRate: 20
         });
         
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('elfa', { start: 1, end: 26 }),
-            frameRate: 24,
+            frameRate: 20,
             repeat: -1
         });
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('elfa', { start: 27, end: 53 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jumpRight',
+            frames: this.anims.generateFrameNumbers('elfa', { start: 54, end: 54 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jumpLeft',
+            frames: this.anims.generateFrameNumbers('elfa', { start: 55, end: 55 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
 
 
          //ANIMATIONS DEL GATO
@@ -67,25 +91,48 @@ class Elfo extends Phaser.GameObjects.Sprite{
         if(!this.metamorfosis){ //modo persona
             if (this.flechas.left.isDown)
             {
+                this.lookLeft = true;
                 this.body.setVelocityX(-400);
             
                 this.elfo.anims.play("left", true);
+
+                
             }
             else if (this.flechas.right.isDown)
             {
+                this.elfo.lookLeft = false;
                 this.body.setVelocityX(400);
                 this.elfo.anims.play('right', true);
+
+                
             }
-            else
+            else if (this.lookLeft == true)
             {
                 this.body.setVelocityX(0);
-                this.elfo.anims.play('turn');
+                this.elfo.anims.play('turnLeft', true);
+            }
+            else 
+            {
+                this.body.setVelocityX(0);
+                this.elfo.anims.play('turnRight', true);
             }
     
             //DOBLE SALTO HECHO GRACIAS A: https://www.youtube.com/watch?v=j1Y75isZ1oM
-            if (Phaser.Input.Keyboard.JustDown(this.flechas.up) && (this.elfo.body.blocked.down || this.canDoubleJump<=1)) //
+            if ((Phaser.Input.Keyboard.JustDown(this.flechas.up) || this.flechas.up.isDown ) && (this.elfo.body.blocked.down || this.canDoubleJump<=1)) //
             { 
                 this.SonidoSalto.play();
+
+                if(this.lookLeft == false){
+                    console.log(this.elfo.lookLeft);
+                    this.elfo.anims.play('jumpRight', true);
+                }
+                else
+                {
+                    this.elfo.anims.play('jumpLeft',true);
+                    console.log(this.elfo.lookLeft);
+                }
+                
+                
                 if(this.canDoubleJump==0){
                 this.body.setVelocityY(-600); 
                 ++this.canDoubleJump;
--- javascript/GameObjects/Gnomo.js
+++ javascript/GameObjects/Gnomo.js
@@ -9,6 +9,8 @@ class Gnomo extends Phaser.GameObjects.Sprite{
 
         this.wasd = scene.input.keyboard.addKeys({ up: 'W', left: 'A', down: 'S', right: 'D', e: "E", q: "Q"});
 
+        this.gnomo.setScale(0.3);
+
         this.body.setCollideWorldBounds(true);
 
         this.SonidoSalto = this.scene.sound.add("Sonido_Salto");
@@ -18,14 +20,20 @@ class Gnomo extends Phaser.GameObjects.Sprite{
         //ANIMATIONS DEL MUÑECO
         this.anims.create({
             key: 'walk',
-            frames: this.anims.generateFrameNumbers('enano', { start: 1, end: 15 }),
+            frames: this.anims.generateFrameNumbers('enano', { start: 0, end: 15 }),
             frameRate: 20,
             repeat: -1
         });
         
         this.anims.create({
             key: 'turn',
-            frames: [ { key: 'enano', frame: 0 } ],
+            frames: [ { key: 'enano', frame: 16 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'jump',
+            frames: [ { key: 'enano', frame: 16 } ],
             frameRate: 20
         });
     }
@@ -33,7 +41,7 @@ class Gnomo extends Phaser.GameObjects.Sprite{
 
     move(){
         //-------MOVIMIENTO GNOMO-------
-        if (this.wasd.left.isDown)
+        if (this.wasd.left.isDown && !this.wasd.up.isDown)
         {
             this.body.setVelocityX(-400);
 
@@ -61,22 +69,23 @@ class Gnomo extends Phaser.GameObjects.Sprite{
             this.SonidoSalto.play();
             this.body.setVelocityY(-600);
             this.canJump = false;
+            this.gnomo.anims.play('jump', true);
         }
         else if(this.gnomo.body.blocked.down){ //this.elfo.body.blocked.down funciona con los tiles. El isTouching no
             this.canJump = true;
         }
 
         //PREGUNTARLE ESTO
-        if (this.wasd.q.isDown && this.gnomo.scale == 0.5)
+        if (this.wasd.q.isDown && this.gnomo.scale == 0.15)
         {
             this.gnomo.setPosition(this.gnomo.x,this.gnomo.y-30); //subirle el tamaño del sprite
             //this.body.setVelocityY(-300);
-            this.gnomo.setScale(1);
+            this.gnomo.setScale(0.3);
         }
 
        if (this.wasd.e.isDown && this.gnomo.scale == 1)
         {     
-            this.gnomo.setScale(0.5);
+            this.gnomo.setScale(0.15);
         }   
         
     }
