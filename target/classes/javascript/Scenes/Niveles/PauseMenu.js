17d565ed74df206fe8d7d0942e53fe42505992f8
carlosmartinhernandez79 <147002686+carlosmartinhernandez79@users.noreply.github.com>
2023-12-18 09:36:33 +0100

    Add files via upload

---
+++ target/classes/javascript/Scenes/Niveles/PauseMenu.js
@@ -0,0 +1,149 @@
+class PauseMenu extends Phaser.Scene{
+    constructor() {
+        super({ key: 'PauseMenu' });
+    }
+    init(data){
+        if(data){
+            this.sonido = data.sonido;
+        }
+    }
+    preload() {
+        // Puedes precargar recursos específicos de esta escena aquí
+        console.log("pause")
+    }
+
+    create() {
+        // Lógica de inicialización de la escena
+        const graphics = this.add.graphics();
+
+        graphics.fillStyle(0x000000, 0.5);
+        graphics.fillRect(0, 0, 1200, 600);
+        //graphics.alpha = 0.5     
+
+        this.add.image(600,70, "PausaText").setOrigin(0.5,0.5);
+        this.escape = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+
+
+        this.menuOff = this.add.image(600,200,"menuOff").setScale(0.6);
+        this.menuOn = this.add.image(600,200,"menuOn").setScale(0.7);;
+        
+        this.menu = this.menuOff
+
+        this.menu.setInteractive();
+        this.menuOn.setVisible(false);
+
+        this.menu.on('pointerover',()=>{
+            this.menuOn.setVisible(true);
+        })
+        
+        this.menu.on('pointerout',()=>{
+            this.menuOff.setVisible(true);
+            this.menuOn.setVisible(false);
+        })
+
+        this.menu.on('pointerdown', function () {
+            this.scene.start('StartScreen',{sonido: this.scene.get("TutorialLevel").getSound()});
+            this.scene.bringToTop('StartScreen');
+            this.scene.sendToBack('TutorialLevel');
+            this.scene.sleep('PauseMenu');
+            this.scene.sendToBack('OptionsFromPause');
+            this.scene.sendToBack('Tiempo_Monedas');
+        }, this);
+
+
+
+        //--------------------------------------------------
+        this.opcionesOff = this.add.image(600,300,"opcionesOff").setScale(0.6);
+        this.opcionesOn = this.add.image(600,300,"opcionesOn").setScale(0.7);
+        
+        this.opciones = this.opcionesOff
+
+        this.opciones.setInteractive();
+        this.opcionesOn.setVisible(false);
+
+        this.opciones.on('pointerover',()=>{
+            this.opcionesOn.setVisible(true);
+        })
+        
+        this.opciones.on('pointerout',()=>{
+            this.opcionesOff.setVisible(true);
+            this.opcionesOn.setVisible(false);
+        })
+
+        this.opciones.on('pointerdown', function () {
+            console.log(this.scene.get("StartScreen").isMusicOn()+ ": FROM PAUSE MENU")
+            this.scene.start('OptionsFromPause', {sonido: this.scene.get("StartScreen").isMusicOn()});
+            this.scene.bringToTop('OptionsFromPause'); //mostramos sobre todas esta escena
+            this.scene.pause('PauseMenu'); //dormimos la otra, porque no queremos perder lo que hagamos en el menu de pausa
+            this.scene.sendToBack('TutorialLevel'); //enviamos al fondo la de tutorial
+            //this.scene.sendToBack('Tiempo_Monedas');
+        }, this);
+
+        //----------------------------------------------
+        this.reiniciarOff = this.add.image(600,400,"reiniciarOff").setScale(0.6);
+        this.reiniciarOn = this.add.image(600,400,"reiniciarOn").setScale(0.7);;
+        this.reiniciar = this.reiniciarOff
+        this.reiniciarOn.setVisible(false);
+        this.reiniciar.setInteractive()
+
+      
+        this.reiniciar.on('pointerover',()=>{
+            this.reiniciarOn.setVisible(true);
+        })
+        
+        this.reiniciar.on('pointerout',()=>{
+            this.reiniciarOff.setVisible(true);
+            this.reiniciarOn.setVisible(false);
+        })
+
+        this.reiniciar.on('pointerdown', function () {
+            this.scene.start('TutorialLevel');
+        }, this);
+
+        //-------------------------------------------------
+
+        this.nivelesOff = this.add.image(600,500,"nivelesOff").setScale(0.6);
+        this.nivelesOn = this.add.image(600,500,"nivelesOn").setScale(0.7);
+        this.niveles = this.nivelesOff;
+
+        this.nivelesOn.setVisible(false);
+        this.niveles.setInteractive()
+
+        this.niveles.on('pointerover',()=>{
+            this.nivelesOn.setVisible(true);
+        })
+        
+        this.niveles.on('pointerout',()=>{
+            this.nivelesOff.setVisible(true);
+            this.nivelesOn.setVisible(false);
+        })
+
+        this.niveles.on('pointerdown', function () {
+            this.scene.start('LevelSelector');
+            this.scene.bringToTop('LevelSelector');
+            this.scene.sendToBack('TutorialLevel');
+            this.scene.sendToBack('Tiempo_Monedas');
+        }, this);
+    }
+
+
+    
+    /*button.on('pointerover',function(pointer){
+        button.setFrame(1);
+    })
+    
+    button.on('pointerout',function(pointer){
+        button.setFrame(0);
+    })*/
+
+    update() {
+        if(this.escape.isDown){
+            this.scene.sleep();
+            console.log("Resume")
+            this.scene.resume("Tiempo_Monedas");
+            var sceneMain = this.scene.resume("TutorialLevel");
+            this.scene.get("TutorialLevel").checkSound()
+
+        }
+    }
+}
---
+++ target/classes/javascript/Scenes/Niveles/Victory.js
@@ -0,0 +1,35 @@
+class Victory extends Phaser.Scene{
+    constructor() {
+        super({ key: 'Victory' });
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        this.scene.bringToTop();
+        const graphics = this.add.graphics();
+        this.scene.get("TutorialLevel").MiMusicaBase.pause();
+
+        this.add.image(0,0, "PantallaVictoria").setOrigin(0,0);
+        
+        this.add.text(850, 340, this.scene.get("Tiempo_Monedas").getTime(), {font: "30px Arial", fill: "white"})
+
+        this.add.text(520, 340, this.scene.get("Tiempo_Monedas").getMonedas()+ "/3", {font: "30px Arial", fill: "white"})
+
+        this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+
+    }
+
+    update() {
+        if(this.space.isDown){
+            this.scene.stop();
+            this.scene.stop("Tiempo_Monedas");
+            var sceneMain = this.scene.start("LevelSelector");
+            this.scene.bringToTop("LevelSelector");
+            this.scene.get("TutorialLevel").MiMusicaBase.pause();
+        }
+    }
+}
